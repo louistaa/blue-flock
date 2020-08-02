ebe2ff6ec57495e17647164eceb9b945cac95678
Ana Carolina Arellano <anacarolinaarellano@gmail.com>
2020-08-02 10:26:57 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'LY66zhOzv53aZm7lBRibx94ns',
-  consumer_secret:      'FeHT3PY24nURlBMrU3QzWYRGOpVQ1mTfxJqle7duua559CaChq',
-  access_token:         '1105793491576090624-zYNN9Vy0O1fDaS9U5XccQWAqhuZKi1',
-  access_token_secret:  'P35onKoDXfEfZ1jJQgBv5wJTrWhkXDZ7mlvtebWoK2OSA',
-}
