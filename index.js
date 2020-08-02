4e95991459554acd934e055f384e4be0536cb5ea
Alex Gist <alexgist@MacBook-Air-2.local>
2020-08-01 22:23:39 -0700

    moved keys to config

--- index.js
+++ index.js
@@ -85,12 +85,6 @@ async function didMention(event) {
       let Twit = require("twit");
 
       let T = new Twit(config);
-      /*let T = new Twit({
-        consumer_key: "LY66zhOzv53aZm7lBRibx94ns",
-        consumer_secret: "FeHT3PY24nURlBMrU3QzWYRGOpVQ1mTfxJqle7duua559CaChq",
-        access_token: "1105793491576090624-zYNN9Vy0O1fDaS9U5XccQWAqhuZKi1",
-        access_token_secret: "P35onKoDXfEfZ1jJQgBv5wJTrWhkXDZ7mlvtebWoK2OSA",
-      });*/
 
       T.post("statuses/update", { status: statusMessageToSend }, function (
         err,
